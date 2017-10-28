3ffe93fa3a103a7100c4c445f4310fbf9020b90f
Brad Jacobs <brad.jacobs@galvanize.com>
2017-10-28 07:59:34 -0700

    prep all_hands_analysis for aces and eights

--- all_hands_analysis.py
+++ all_hands_analysis.py
@@ -1,3 +1,4 @@
+from functools import partial
 from itertools import combinations, product
 from vp_analyzer import HandAnalyzer
 import time
@@ -26,31 +27,32 @@ def analyze_some_hands(hands_gen, n = 10000):
 
     return analysis
 
-def analyze_hand(handstr):
-    #handstr = hand2str(hand_tup)
-    results = HandAnalyzer(handstr).analyze()
-    max_ev = 0
-    best_ev_disc = 0
-    for holddisc in results:
-        cur_ev = results[holddisc]['expected_val']
+def analyze_hand(handstr, payouts = None):
+    ## moved to vp_analyzer
+    # #handstr = hand2str(hand_tup)
+    # results = HandAnalyzer(handstr).analyze()
+    # max_ev = 0
+    # best_ev_disc = 0
+    # for holddisc in results:
+    #     cur_ev = results[holddisc]['expected_val']
+    #
+    #     equal_bool = () and (cur_disc > best_ev_disc)
+    #     if cur_ev > max_ev:
+    #         besthold = holddisc
+    #         max_ev = cur_ev
+    #         best_ev_disc = holddisc.count('XX')
+    #     elif cur_ev == max_ev:
+    #         cur_ev_disc = holddisc.count('XX')
+    #         if cur_ev_disc > best_ev_disc:
+    #             besthold = holddisc
+    #             best_ev_disc = cur_ev_disc
+    #     else:
+    #         continue
+    #
+    hand = HandAnalyzer(handstr, payouts=payouts)
+    results = hand.analyze(return_full_analysis=False)
+    return '{},{},{}'.format(handstr, *results)
 
-        equal_bool = () and (cur_disc > best_ev_disc)
-        if cur_ev > max_ev:
-            besthold = holddisc
-            max_ev = cur_ev
-            best_ev_disc = holddisc.count('XX')
-        elif cur_ev == max_ev:
-            cur_ev_disc = holddisc.count('XX')
-            if cur_ev_disc > best_ev_disc:
-                besthold = holddisc
-                best_ev_disc = cur_ev_disc
-        else:
-            continue
-
-
-    bestholdstr = ''.join(besthold)
-    line = '{}, {}, {}'.format(handstr, bestholdstr, str(results[besthold]['expected_val']))
-    return line
 
 def limit_hands_wrapper(hands_gen, n = 10000):
     for ind, hand in enumerate(hands_gen):
@@ -59,15 +61,21 @@ def limit_hands_wrapper(hands_gen, n = 10000):
         else:
             yield hand
 
-def save_chunks(hands_lst, filename_base, chunksize = 100000):
+def save_chunks(hands_lst, filename_base, payouts = None, chunksize = 100000):
     procs = multiprocessing.cpu_count()
+    if payouts is None:
+        mapfunc = analyze_hand
+    else:
+        kwargs = {'payouts': payouts}
+        mapfunc = partial(analyze_hand, **kwargs)
+
     for ind in range(0, len(hands_lst), chunksize):
 
         pool = multiprocessing.Pool(processes = procs)
         if ind+chunksize <= len(hands_lst):
-            hands_analysis = pool.map(analyze_hand, hands_lst[ind:ind+chunksize])
+            hands_analysis = pool.map(mapfunc, hands_lst[ind:ind+chunksize])
         else:
-            hands_analysis = pool.map(analyze_hand, hands_lst[ind:])
+            hands_analysis = pool.map(mapfunc, hands_lst[ind:])
 
         fname = filename_base + str(ind) + '.txt'
         with open(fname, 'w') as fout:
@@ -89,7 +97,18 @@ if __name__ == '__main__':
     #analysis = pool.map(analyze_hand, limit_hands_wrapper(all_hands_gen(), n = 10))
     #print(analysis)
     #print([ha[('X','X','X','X','X')]['expected_val'] for ha in analysis])
-
+    aces8s_d = {'flush': 5,
+                  'four_kind': 25,
+                  'four_kind7': 50,
+                  'four_kindA8': 80,
+                  'full_house': 8,
+                  'pair_jqka': 1,
+                  'royal_flush': 800,
+                  'straight': 4,
+                  'straight_flush': 50,
+                  'three_kind': 3,
+                  'two_pair': 2}
     all_hands_str_l = list(map(hand2str, all_hands_gen()))
-    indout = save_chunks(all_hands_str_l, 'poker_hand_evs_', chunksize = 100000)
+    indout = save_chunks(all_hands_str_l, 'poker_hand_evs_test',
+                         payouts = aces8s_d, chunksize = 1000)
     print(time.localtime())
