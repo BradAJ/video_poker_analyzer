2c9bd19a9a455d5938b7a29996934b177917a2ac
Brad Jacobs <brad.jacobs@galvanize.com>
2017-11-11 12:38:29 -0800

    remove old simulate_threes_given_pair. clean up all_hands_analysis

--- all_hands_analysis.py
+++ all_hands_analysis.py
@@ -4,6 +4,14 @@ from vp_analyzer import HandAnalyzer
 import time
 import multiprocessing
 
+"""
+Script to generate a series of files that together contain all ~2.6M five-card
+poker hands and the optimal discard for a given video poker payout table, along
+with the expected value of that play.
+
+Written to be run in parallel across multiple cores via multiprocessing.
+"""
+
 def all_hands_gen():
     ranks = 'A23456789TJQK'
     suits = 'CDHS'
@@ -17,51 +25,38 @@ def hand2str(hand_tup):
         hstr += r + s
     return hstr
 
-def analyze_some_hands(hands_gen, n = 10000):
-    analysis = []
-    for ind, hand in enumerate(hands_gen):
-        if ind >= n:
-            break
-        handstr = hand2str(hand)
-        analysis.append(HandAnalyzer(handstr).analyze())
-
-    return analysis
-
 def analyze_hand(handstr, payouts = None):
-    ## moved to vp_analyzer
-    # #handstr = hand2str(hand_tup)
-    # results = HandAnalyzer(handstr).analyze()
-    # max_ev = 0
-    # best_ev_disc = 0
-    # for holddisc in results:
-    #     cur_ev = results[holddisc]['expected_val']
-    #
-    #     equal_bool = () and (cur_disc > best_ev_disc)
-    #     if cur_ev > max_ev:
-    #         besthold = holddisc
-    #         max_ev = cur_ev
-    #         best_ev_disc = holddisc.count('XX')
-    #     elif cur_ev == max_ev:
-    #         cur_ev_disc = holddisc.count('XX')
-    #         if cur_ev_disc > best_ev_disc:
-    #             besthold = holddisc
-    #             best_ev_disc = cur_ev_disc
-    #     else:
-    #         continue
-    #
     hand = HandAnalyzer(handstr, payouts=payouts)
     results = hand.analyze(return_full_analysis=False)
     return '{},{},{}'.format(handstr, *results)
 
 
-def limit_hands_wrapper(hands_gen, n = 10000):
-    for ind, hand in enumerate(hands_gen):
-        if ind >= n:
-            break
-        else:
-            yield hand
-
 def save_chunks(hands_lst, filename_base, payouts = None, chunksize = 100000):
+    """
+    Wrapper func for spreading analysis work across available cores, and saving
+    intermediate results rather than waiting to write out the results of all
+    hands in a single file.
+    Why?:
+    Analysis of a given hand's 32 possible discards takes between 0.01 and 0.1s
+    with a single core on my machine, so depending on setup, analysis of all
+    possible hands will likely take several hours.
+
+    INPUT:
+    hands_lst: (list of str) List of 10-char strings of poker hands.
+    filename_base: (str) base name of text files to be written. these will be
+                    appended with the lowest count of chunksize and '.txt'. e.g.
+                    filename_base = "poker_hands_" writes out poker_hands_0.txt,
+                    poker_hands_100000.txt, etc. (if chunksize = default)
+    payouts: (dict) Payout value of each winning hand.
+                    If None, see: vp_analyzer.HandAnalyzer, default is for
+                    "9-6 Jacks or Better"
+    chunksize: (int) Number of hands to collect in each output file. Usually set
+                     to be some sizeable fraction of len(hands_lst).
+
+    OUTPUT:
+    Files to disk: (text)
+
+    """
     procs = multiprocessing.cpu_count()
     if payouts is None:
         mapfunc = analyze_hand
@@ -82,21 +77,12 @@ def save_chunks(hands_lst, filename_base, payouts = None, chunksize = 100000):
             # write results on separate lines, include \n on last line
             fout.write('\n'.join(hands_analysis)+'\n')
         print('Saved: {}'.format(fname))
-    return str(ind)
+
 
 
 if __name__ == '__main__':
     print(time.localtime())
 
-    #all_hands = all_hands_gen()
-    #print(len(analyze_some_hands(all_hands)))
-    #print([x for x in limit_hands_wrapper(all_hands_gen(), n = 10)])
-
-
-    #pool = multiprocessing.Pool(processes = procs)
-    #analysis = pool.map(analyze_hand, limit_hands_wrapper(all_hands_gen(), n = 10))
-    #print(analysis)
-    #print([ha[('X','X','X','X','X')]['expected_val'] for ha in analysis])
     aces8s_d = {'flush': 5,
                   'four_kind': 25,
                   'four_kind7': 50,
